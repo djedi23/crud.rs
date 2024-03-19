f8d34f7f29d5ba7bbd12e52b8813eded866a970f
Moïse Valvassori <moise.valvassori@gmail.com>
2024-03-19 07:14:21 +0100

    feat(crud-api): refactoring http client to hyper v1

--- crud-api/src/http.rs
+++ crud-api/src/http.rs
@@ -1,22 +1,27 @@
 use crate::{error::ApiError, Query};
 use async_trait::async_trait;
 use crud_auth::CrudAuth;
-use hyper::{
-  body::{aggregate, Buf, HttpBody},
-  client::HttpConnector,
-  Body, Client, Method, Request, StatusCode,
-};
-use hyper_tls::HttpsConnector;
+use http::uri::Authority;
+use http_body_util::BodyExt;
+use hyper::client::conn::http1::SendRequest;
+use hyper::{body::Buf, Method, Request, StatusCode, Uri};
+use hyper_util::rt::TokioIo;
 use indicatif::{ProgressBar, ProgressStyle};
 use log::trace;
 use miette::{miette, IntoDiagnostic, Result, WrapErr};
 use serde::{de::DeserializeOwned, Serialize};
 use std::{
-  collections::HashMap, fmt::Debug, io::Read, marker::PhantomData, path::Path, time::Duration,
+  collections::HashMap, fmt::Debug, io::Read, marker::PhantomData, path::Path, sync::Arc,
+  time::Duration,
 };
 use tokio::{
   fs::{create_dir_all, File},
   io::{stdout, AsyncWriteExt},
+  net::TcpStream,
+};
+use tokio_rustls::{
+  rustls::{ClientConfig, RootCertStore},
+  TlsConnector,
 };
 
 pub struct HTTPApi<'a> {
@@ -54,9 +59,80 @@ pub struct Header<'a> {
   pub value: &'a str,
 }
 
-fn get_client() -> Client<HttpsConnector<HttpConnector>> {
-  let https = HttpsConnector::new();
-  Client::builder().build::<_, hyper::Body>(https)
+async fn connect(uri: &str) -> Result<(SendRequest<String>, Authority)> {
+  let url: Uri = uri
+    .parse()
+    .into_diagnostic()
+    .context("Error during URL parsing")?;
+
+  let host = url.host().expect("uri has no host");
+  let port = url.port_u16().unwrap_or({
+    if let Some(scheme) = url.scheme() {
+      trace!("scheme {:?}", scheme);
+      if scheme == &http::uri::Scheme::HTTPS {
+        443
+      } else {
+        80
+      }
+    } else {
+      80
+    }
+  });
+  let addr = format!("{}:{}", host, port);
+
+  let mut root_cert_store = RootCertStore::empty();
+  root_cert_store.extend(webpki_roots::TLS_SERVER_ROOTS.iter().cloned());
+  let config = ClientConfig::builder()
+    .with_root_certificates(root_cert_store)
+    .with_no_client_auth();
+  let connector = TlsConnector::from(Arc::new(config));
+
+  let domain = url.host().unwrap();
+  let domain = pki_types::ServerName::try_from(domain)
+    .into_diagnostic()?
+    .to_owned();
+
+  let stream = TcpStream::connect(addr).await.into_diagnostic()?;
+  let sender = if let Some(scheme) = url.scheme() {
+    if scheme == &http::uri::Scheme::HTTPS {
+      let io = TokioIo::new(connector.connect(domain, stream).await.into_diagnostic()?);
+      let (sender, conn) = hyper::client::conn::http1::handshake(io)
+        .await
+        .into_diagnostic()?;
+      tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+          println!("Connection failed: {:?}", err);
+        }
+      });
+      sender
+    } else {
+      let io = TokioIo::new(stream);
+      let (sender, conn) = hyper::client::conn::http1::handshake(io)
+        .await
+        .into_diagnostic()?;
+      tokio::task::spawn(async move {
+        if let Err(err) = conn.await {
+          println!("Connection failed: {:?}", err);
+        }
+      });
+      sender
+    }
+  } else {
+    let io = TokioIo::new(stream);
+    let (sender, conn) = hyper::client::conn::http1::handshake(io)
+      .await
+      .into_diagnostic()?;
+    tokio::task::spawn(async move {
+      if let Err(err) = conn.await {
+        println!("Connection failed: {:?}", err);
+      }
+    });
+    sender
+  };
+  // The authority of our URL will be the hostname of the httpbin remote
+  let authority = url.authority().unwrap().clone();
+
+  Ok((sender, authority))
 }
 
 #[async_trait]
@@ -73,11 +149,12 @@ impl Query for HTTPApi<'_> {
     R: Send + DeserializeOwned + Debug + Default,
     Q: Send + Serialize + Debug,
   {
-    let client = get_client();
     let mut uri = self.uri.to_owned();
     if let Some(qa) = query_args {
       uri = format!("{}?{}", uri, serde_qs::to_string(&qa).unwrap());
     }
+    let (mut sender, authority) = connect(&uri).await?;
+
     let req = Request::builder().method(&self.method).uri(&uri);
     trace!("Request {} to {}", self.method, uri);
     let req = if let Some(auth) = self.auth {
@@ -97,44 +174,38 @@ impl Query for HTTPApi<'_> {
 
     let req = req
       .header("content-type", "application/json; charset=UTF-8")
+      .header(hyper::header::HOST, authority.as_str())
       .body(match payload {
-        Some(ref payload) => Body::from({
+        Some(ref payload) => {
           let p = serde_json::to_string(&payload)
             .into_diagnostic()
             .context("Error during payload serialization")?;
           trace!("Payload: {}", p);
           p
-        }),
-        None => Body::empty(),
+        }
+        None => String::new(),
       })
       .into_diagnostic()
       .with_context(|| format!("Payload: {payload:?}"))
       .with_context(|| format!("URL: {uri}"))
       .context("HTTP request preparation failed.")?;
 
-    let response = client
-      .request(req)
+    let response = sender
+      .send_request(req)
       .await
       .into_diagnostic()
       .with_context(|| format!("Payload: {payload:?}"))
       .with_context(|| format!("URL: {uri}"))
       .context("HTTP call fail")?;
     trace!("Response status: {}", response.status());
-    // {
-    // use hyper::body::HttpBody;
-    // use tokio::io::{stdout, AsyncWriteExt as _};
-    // while let Some(chunk) = response.body_mut().data().await {
-    //   stdout()
-    //     .write_all(&chunk.into_diagnostic()?)
-    //     .await
-    //     .into_diagnostic()?;
-    // }
     if response.status() == self.ok_status {
-      let body = aggregate(response)
+      let body = response
+        .collect()
         .await
         .into_diagnostic()
         .with_context(|| format!("URL: {uri}"))
-        .context("Can't read the HTTP response")?;
+        .context("Can't read the HTTP response")?
+        .aggregate();
 
       if !body.has_remaining() {
         Ok(R::default()) // I don't find a type that can deserialize an empty string.
@@ -183,14 +254,18 @@ impl Query for HTTPApi<'_> {
         .unwrap_or(&empty_string)
         .to_string();
 
+      trace!("Response {:?}", response);
+
       let status = response.status();
 
       let mut error_body = String::new();
-      aggregate(response)
+      response
+        .collect()
         .await
         .into_diagnostic()
         .with_context(|| format!("URL: {uri}"))
         .wrap_err("Can't read the HTTP error response")?
+        .aggregate()
         .reader()
         .read_to_string(&mut error_body)
         .into_diagnostic()
@@ -220,8 +295,9 @@ impl Query for HTTPApi<'_> {
     P: Send + Serialize + Debug,
     Q: Send + Serialize + Debug,
   {
-    let client = get_client();
     let mut uri = self.uri.to_owned();
+    let (mut sender, authority) = connect(&uri).await?;
+
     if let Some(qa) = query_args {
       uri = format!("{}?{}", uri, serde_qs::to_string(&qa).unwrap());
     }
@@ -243,23 +319,24 @@ impl Query for HTTPApi<'_> {
     }
     let req = req
       .header("content-type", "application/json")
+      .header(hyper::header::HOST, authority.as_str())
       .body(match payload {
-        Some(ref payload) => Body::from({
+        Some(ref payload) => {
           let p = serde_json::to_string(&payload)
             .into_diagnostic()
             .context("Error during payload serialization")?;
           trace!("Payload: {}", p);
           p
-        }),
-        None => Body::empty(),
+        }
+        None => String::new(),
       })
       .into_diagnostic()
       .with_context(|| format!("Payload: {payload:?}"))
       .with_context(|| format!("URL: {uri}"))
       .context("HTTP request preparation failed.")?;
 
-    let mut response = client
-      .request(req)
+    let mut response = sender
+      .send_request(req)
       .await
       .into_diagnostic()
       .with_context(|| format!("Payload: {payload:?}"))
@@ -302,28 +379,32 @@ impl Query for HTTPApi<'_> {
           .into_diagnostic()
           .with_context(|| format!("Failed to create file: {}", path.display()))?;
 
-        while let Some(chunk) = response.data().await {
-          let chunk = chunk
+        while let Some(chunk) = response.frame().await {
+          let frame = chunk
             .into_diagnostic()
             .with_context(|| format!("URL: {uri}"))?;
-          bar.inc(chunk.len().try_into().into_diagnostic()?);
-          file
-            .write_all(&chunk)
-            .await
-            .into_diagnostic()
-            .with_context(|| format!("Failed to write file: {}", path.display()))?;
+          if let Some(chunk) = frame.data_ref() {
+            bar.inc(chunk.len().try_into().into_diagnostic()?);
+            file
+              .write_all(chunk)
+              .await
+              .into_diagnostic()
+              .with_context(|| format!("Failed to write file: {}", path.display()))?;
+          }
         }
       } else {
-        while let Some(chunk) = response.data().await {
-          let chunk = chunk
+        while let Some(chunk) = response.frame().await {
+          let frame = chunk
             .into_diagnostic()
             .with_context(|| format!("URL: {uri}"))?;
-          bar.inc(chunk.len().try_into().into_diagnostic()?);
-          stdout()
-            .write_all(&chunk)
-            .await
-            .into_diagnostic()
-            .context("Failed to write: {}")?;
+          if let Some(chunk) = frame.data_ref() {
+            bar.inc(chunk.len().try_into().into_diagnostic()?);
+            stdout()
+              .write_all(chunk)
+              .await
+              .into_diagnostic()
+              .context("Failed to write: {}")?;
+          }
         }
       };
       bar.finish_and_clear();
@@ -338,11 +419,13 @@ impl Query for HTTPApi<'_> {
 
       let status = response.status();
       let mut error_body = String::new();
-      aggregate(response)
+      response
+        .collect()
         .await
         .into_diagnostic()
         .with_context(|| format!("URL: {uri}"))
         .wrap_err("Can't read the HTTP error response")?
+        .aggregate()
         .reader()
         .read_to_string(&mut error_body)
         .into_diagnostic()
